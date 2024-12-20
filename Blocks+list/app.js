b8fa5bb737aa46b3fdbba1d755a8f1a379ab8ce3
Ryan Benddif <R.Benddif2645@student.leedsbeckett.ac.uk>
2024-12-20 15:02:58 +0000

    updated how Exploit list blocks work and its output

--- Blocks+list/app.js
+++ Blocks+list/app.js
@@ -1,27 +1,30 @@
 
-var	example3Left = document.getElementById('ExploitList'),
-	example3Right = document.getElementById('XML_Classes')
+var	example3Left = document.getElementById('ExploitList'), example3Right = document.getElementById('XML_Classes')
 
+function addText(text) {
+	document.getElementById('xml-text').value += text + "\n\n" + text;
+}	
 	
-	
-// Example 3 - Cloning
+// This is the exploit list it cannot be added to and the sorting wont change
 new Sortable(ExploitList, {
 	group: {
+		filter: '.Header',
 		name: 'shared',
-		pull: 'clone' // To clone: set pull to 'clone'
+		pull: 'clone', // To clone: set pull to 'clone'
+		put: false,
 	},
-	animation: 150
+	animation: 150,
+	sort: false
 });
 
+// This is Class diagram 
 new Sortable(XML_Classes, {
 	group: {
+		filter: '.Header',
 		name: 'shared',
-		pull: 'clone',
 		sort: true
 	},
 	animation: 150
 });
 
-function addText(text) {
-	document.getElementById('xml-text').value += text;
-}
+
